6162f36a8e2b375d37bbfcadb5aa86ab11c59411
James <metal.lunchbox@gmail.com>
2019-07-20 08:38:38 +0800

    minor changes

--- src/components/NotFound.svelte
+++ src/components/NotFound.svelte
@@ -1,6 +1,5 @@
 <script>
-  const path = window.location.pathname
-  const goback = () => window.history.back()
+  const path = window.location.pathname;
 </script>
 <style>
   .container {
@@ -21,11 +20,12 @@
 
 <div class="container">
   <section>
-    <h1 class="title is-1 primary"><i class="fas fa-frown-open"></i>&nbsp;404</h1>
+    <h1 class="title is-1 primary">
+      <i class="fas fa-frown-open"></i>&nbsp;404
+    </h1>
     <h2 class="subtitle is-3">resource not found</h2>
 
     <p>The path '{path}' leads nowhere</p>
     <p>It might be wise to return to the <a href="/">main page</a>.</p>
-    <button on:click={goback} class="button is-link"><i class="fas fa-backward"></i>&nbsp;go back</button>
   </section>
 </div>
